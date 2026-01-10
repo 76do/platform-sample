df84c9c9c76317d17b0222d35741b11c55fdeab9
石田裕輔 <ishida@idealog.co.jp>
2026-01-10 16:46:07 +0900

    test deploy

--- app/test/[id]/_component/sleep.tsx
+++ app/test/[id]/_component/sleep.tsx
@@ -1,7 +1,7 @@
 import { cacheTag } from "next/cache";
 
 export default async function Sleep({ id }: { id: string }) {
-  "use cache: remote";
+  "use cache";
   cacheTag(`tag-${id}`);
 
   await new Promise((resolve) => setTimeout(resolve, 10000));
