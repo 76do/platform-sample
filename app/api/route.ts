f9de34519a3ce1e2e656a6a2b19d7c357482fb91
石田裕輔 <ishida@idealog.co.jp>
2026-01-10 15:32:35 +0900

    first deploy

---
+++ app/api/route.ts
@@ -0,0 +1,8 @@
+import { revalidateTag } from 'next/cache';
+
+export async function GET(request: Request) {
+	const { searchParams } = new URL(request.url);
+	const name = searchParams.get('name') || 'default-tag';
+	revalidateTag(name, 'max');
+	return new Response('Revalidation triggered');
+}
